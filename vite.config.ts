29745fe5168eecc246e707ddf5e246ed2a475ac8
Hashim Gendi <hgendi3@gmail.com>
2026-08-11 06:32:15 +0300

    Configure cloudflare-pages preset in Nitro to resolve ASSETS binding error

--- vite.config.ts
+++ vite.config.ts
@@ -12,4 +12,14 @@ export default defineConfig({
     // nitro/vite builds from this
     server: { entry: "server" },
   },
+  nitro: {
+    preset: "cloudflare-pages",
+    cloudflare: {
+      wrangler: {
+        assets: {
+          binding: "STATIC_ASSETS",
+        },
+      },
+    },
+  },
 });
